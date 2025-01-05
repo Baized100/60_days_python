de15df45799dd1ef45bfebd0e6ecd93c9a071667
Md.Baizid <111190288+Baized100@users.noreply.github.com>
2025-01-05 20:02:06 +0600

    Add files via upload

---
+++ list in python(10 day).py
@@ -0,0 +1,21 @@
+list=[1,2,3,4]
+print(list)
+print(type(list))
+print(list[1])
+print(list[-1])
+print(list[1:3])
+list[0]="bangladesh"
+print(list)
+list.append("English")
+print(list)
+list.pop(2)
+print(list)
+#input list
+'''li=[]
+n=int(input("enter the index"))
+for i in range(n):
+    new=input()
+    li.append(new)
+    print(li)'''
+c=list+[7,8,9]
+print(c)
